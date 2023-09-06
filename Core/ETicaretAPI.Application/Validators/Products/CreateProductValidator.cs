e075547be4d47d3f0f48bd09dfe780d78ef8ada3
Mehmet-Kemal-Aslan <mehmetkemalaslan@gmail.com>
2023-09-06 23:04:46 +0300

    the twelfth commit

---
+++ Core/ETicaretAPI.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,25 @@
+﻿using ETicaretAPI.Application.ViewModels.Products;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Validators.Products
+{
+    public class CreateProductValidator: AbstractValidator<VM_Create_Product>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty().NotNull().WithMessage("Lütfen ürün adını giriniz.").
+                MaximumLength(150).MinimumLength(2).WithMessage("Ürün adı 2 - 150 karakter olmalıdır");
+
+            RuleFor(p => p.Stock).NotEmpty().NotNull().WithMessage("Lütfen stok bilgisi giriniz.").
+                Must(s => s >= 0).WithMessage("Stok sıfırdan küçük olamaz.");
+
+            RuleFor(p => p.Price).NotEmpty().NotNull().WithMessage("Lütfen fiyat bilgisi giriniz.").
+                Must(p => p >= 0).WithMessage("Fiyat sıfırdan küçük olamaz.");
+        }
+    }
+}
