0ab982f9f82dbb8b7a06858ca0caa208e53d3df4
Mehmet-Kemal-Aslan <mehmetkemalaslan@gmail.com>
2023-09-27 21:59:42 +0300

    the eleventh commit

---
+++ Core/ETicaretAPI.Application/Repositories/File/IFilerWriteRepository.cs
@@ -0,0 +1,8 @@
+﻿using F = ETicaretAPI.Domain.Entities
+
+namespace ETicaretAPI.Application.Repositories.File
+{
+    public interface IFilerWriteRepository : IWriteRepository<ETicaretAPI.Domain.Entities.File>
+    {
+    }
+}
